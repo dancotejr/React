3126e1109d1e4648438267bf00d59a8992ec368e
Dancan Junior <ntaiyadancan@gmail.com>
2023-05-12 17:54:38 +0300

    React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import Message from './components/message';
 import './App.css';
-import Myitems from './components/to-do';
+import Todo from './components/to-do';
 
 
 
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <h1><b><em>My Items</em></b></h1>
-<Myitems />
+<Todo />
     
       
     </div>
